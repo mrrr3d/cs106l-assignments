69b75e5c791551d692f8badd529ab584d1087df2
mrrr61 <mrrr61@outlook.com>
2024-11-29 18:47:17 +0800

    finish assign5

--- assign5/user.cpp
+++ assign5/user.cpp
@@ -1,4 +1,5 @@
 #include "user.h"
+#include <ostream>
 
 /**
  * Creates a new User with the given name and no friends.
@@ -63,3 +64,71 @@ void User::set_friend(size_t index, const std::string& name)
  * STUDENT TODO:
  * The definitions for your custom operators and special member functions will go here!
  */
+
+std::ostream&
+operator << (std::ostream& out, const User& user)
+{
+  out << "User(name=" << user.get_name() << ", friends=[";
+  for (int i = 0;i < user._size ; ++i)
+  {
+    if (i)
+    {
+      out << ", ";
+    }
+    out << user._friends[i];
+  }
+  out << "])";
+  return out;
+}
+
+User::~User()
+{
+  delete [] _friends;
+}
+
+User::User(const User& user)
+{
+  this->_name = user._name;
+  this->_capacity = user._capacity;
+  this->_size = user._size;
+
+  this->_friends = new std::string[this->_capacity];
+  for (int i = 0; i < user._size; ++i)
+  {
+    this->_friends[i] = user._friends[i];
+  }
+}
+
+User&
+User::operator= (const User& user)
+{
+  this->_name = user._name;
+  this->_capacity = user._capacity;
+  this->_size = user._size;
+
+  // don't forget this, free the old value
+  delete[] this->_friends;
+  this->_friends = new std::string[this->_capacity];
+  for (int i = 0; i < user._size; ++i)
+  {
+    this->_friends[i] = user._friends[i];
+  }
+  // return what? doubao helped me.....
+  return *this;
+}
+
+User&
+User::operator+= (User& user)
+{
+  this->add_friend(user.get_name());
+  user.add_friend(this->get_name());
+
+  return *this;
+}
+
+bool
+// add `const' at the end of the line, otherwise error
+User::operator< (const User& user) const
+{
+  return this->get_name() < user.get_name();
+}
